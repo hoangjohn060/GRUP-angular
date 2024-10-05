288817323a9ad6f1109703145209db5d585ebf76
unknown <hoangjohn060@gmail.com>
2024-10-05 22:57:57 +0700

    code new  code

--- src/model/Employee.ts
+++ src/model/Employee.ts
@@ -1,21 +1,22 @@
 export class EmployeeModel {
-
-    empId: number;
+    empid: number;
     name: string;
     city: string;
     state: string;
     emailId: string;
     contactNo: string;
     address: string;
-    pincode:string;
+    pinCode: string;
+
     constructor() {
         this.address = '';
         this.city = '';
-        this.contactNo = '';
-        this.emailId = ''; 
-        this.empId = 1; 
+        this.contactNo= '';
+        this.emailId ='';
+        this.empid = 1;
         this.name = '';
-        this.state='';
-        this.pincode='';
+        this.state = '';
+        this.pinCode = '';
     }
+
 }
